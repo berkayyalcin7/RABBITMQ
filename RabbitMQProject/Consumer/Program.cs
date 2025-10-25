de8154e07ae6ce8dfca6c96d44836c35d4302b7f
Berkay <berkayyalcin7@icloud.com>
2025-10-26 01:08:21 +0300

    first

---
+++ RabbitMQProject/Consumer/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.HostName = "localhost";
+factory.UserName = "admin";
+factory.Password = "admin123";
+factory.VirtualHost = "/";
+
+// Bağlantı oluşturma
+using IConnection connection = await factory.CreateConnectionAsync();
+// Kanal oluşturma
+using IChannel channel = await connection.CreateChannelAsync();
+
+// Queue oluşturma - Publisher'dan gönderilen mesajları almak için kullanılır.
+// Publisher ile aynı yapıda olmalıdır.
+await channel.QueueDeclareAsync(queue: "exampleQueue", exclusive: false, autoDelete: false);
+
+// Mesaj alma
+var consumer = new AsyncEventingBasicConsumer(channel);
+// Mesaj alma işlemi başlatılır.
+// autoAck: true olarak ayarlanırsa, mesajı aldıktan sonra otomatik olarak silinir.
+// false olarak ayarlanırsa, mesajı aldıktan sonra manuel olarak silinir.
+await channel.BasicConsumeAsync(queue: "exampleQueue", false, consumer: consumer);
+
+consumer.ReceivedAsync += async (model, ea) =>
+{
+    // Mesajı byte array'ine çevirir.
+    // Mesajı string'e çevirir.
+    var message = Encoding.UTF8.GetString(ea.Body.Span);
+    // Mesajı ekrana yazdırır.
+    Console.WriteLine("Mesaj alındı: {0}", message);
+};
+// Mesaj alma işlemi beklenir.
+Console.Read();
---
+++ RabbitMQProject/RabbitMQProject.Publisher/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitMQProject.Publisher
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // RabbitMQ bağlantı yapılandırması
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "admin",
+                Password ="admin123",
+                VirtualHost = "/"
+            };
+
+            // Bağalantıyı aktifleştirme ve kanal oluşturma
+            // Dispose etme yapılması gerekiyor.
+            using IConnection connection = await factory.CreateConnectionAsync();
+            // Kanal oluşturma 
+            using IChannel channel = await connection.CreateChannelAsync();
+
+            // Queue oluşturma. exclusive : true olarak ayarlanırsa, başka bir client bu queue'yi kullanamaz. 
+            // autoDelete : true olarak ayarlanırsa, queue boş ise silinir.
+            await channel.QueueDeclareAsync(queue: "exampleQueue", exclusive: false, autoDelete: false);
+
+            // Random mesaj gönderme
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Delay(2000);
+                // Mesaj gönderme - RabbitMQ kuyruğa atacağı mesajları byte array'ine çevirir ve kuyruğa atar.
+                var message = Encoding.UTF8.GetBytes($"Hello, RabbitMQ! {i}");
+                // BasicPublishAsync metodu ile mesajı kuyruğa atar.
+                // exchange: "" olarak ayarlanırsa, direct exchange kullanılır.
+                // routingKey: "exampleQueue" olarak ayarlanırsa, mesajı exampleQueue kuyruğuna atar.
+                // body: message olarak ayarlanırsa, mesajı kuyruğa atar.
+                await channel.BasicPublishAsync(exchange: "", routingKey: "exampleQueue", body: message);
+            }
+            Console.WriteLine("Mesaj gönderildi.");
+
+            Console.Read();
+
+        }
+    }
+}
